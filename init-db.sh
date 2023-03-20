bc21c8789994be95b77eddbdfdc9b0a824edc3ff
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-03-20 08:44:01 +0100

    add: sh file for init of database

---
+++ init-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER docker;
+	CREATE DATABASE matchmaking;
+	GRANT ALL PRIVILEGES ON DATABASE matchmaking TO docker;
+EOSQL
